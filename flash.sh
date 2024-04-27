f5636c91d1b4d03386936eaca5a820520f9aad59
GabeeCoding <gabnology@gmail.com>
2024-04-27 09:35:14 +0100

    clean up/merge flash scripts

--- flash.sh
+++ flash.sh
@@ -1,28 +1,58 @@
-adb shell rm -rf /sdcard/iCupertino/
-source_folder="/Users/berkaytumal/LocalProjects(Mac)/iCupertinoLauncher"
 destination_folder="/sdcard/iCupertino"
-if [ ! -d "$source_folder" ]; then
-  echo "Source folder does not exist!"
-  exit 1
+#node modules seperated by spaces
+NODE_MODULES="jquery css-houdini-squircle"
+
+if [ ! -f package.json ] && [[ ! $* == *--no-check-dir* ]]; then
+    echo 1>&2 "error: missing package.json in $(pwd), check if this is the project root"
+    echo 1>&2 "error: if it is, rerun this script with --no-check-dir"
+    exit 1
 fi
-for item in "$source_folder"/*; do
-  if [ -f "$item" ] && [ "$(basename "$item")" != "node_modules" ]; then
-    adb push "$item" "$destination_folder/"$(basename "$item")
-    echo $item
-  fi
+
+if [ ! -d node_modules ]; then
+    echo 1>&2 "error: node_modules missing, did you forget to run npm install?"
+    exit 1
+fi
+
+devices=$(adb devices | grep -v "List of devices attached" | cut -f 1 | awk NF)
+
+if [ -z $devices ]; then
+    echo 1>&2 "error: no adb devices found!"
+    exit 1
+fi
+
+num_devices=$(echo "$devices" | wc -l)
+
+if [ $num_devices -ge 2 ] && [[ ! $* == *--multi* ]]; then
+    echo 1>&2 "error: multiple devices detected, must specify --multi to flash all"
+    exit 1
+fi
+
+#debug script
+#set -x
+
+for device in $devices; do
+    echo "starting flash for device $device..."
+    echo "removing $destination_folder"
+    adb -s $device shell rm -rf "$destination_folder"
+    echo "done removing $destination_folder"
+
+    adb -s $device shell mkdir "$destination_folder"
+
+    echo "copying files..."
+    for item in *; do
+        item_basename="$(basename "$item")"
+        #node modules will be treated seperately
+        if [ "$item_basename" != "node_modules" ]; then
+            adb -s $device push "$item" "$destination_folder/$item_basename" 1>/dev/null
+        fi
+    done
+    echo "done copying files"
+
+    echo "copying node_modules..."
+    for module in $NODE_MODULES; do
+        adb -s $device push node_modules/$module "$destination_folder/node_modules/$module" 1>/dev/null
+    done
+    echo "done copying node_modules"
+
+    echo "done flash for device $device"
 done
-for item in "$source_folder"/*; do
-  if [ -d "$item" ] && [ "$(basename "$item")" != "node_modules" ]; then
-    adb push "$item" "$destination_folder/"$(basename "$item")
-    echo $item
-  fi
-done
-NODE_MODULES=(
-  "jquery"
-  "css-houdini-squircle"
-)
-for module in "${NODE_MODULES[@]}"; do
-  adb push "/Users/berkaytumal/LocalProjects(Mac)/iCupertinoLauncher/node_modules/$module" "/sdcard/iCupertino/node_modules/$module"
-  echo "/Users/berkaytumal/LocalProjects(Mac)/iCupertinoLauncher/node_modules/$module" to "/sdcard/iCupertino/node_modules/$module"
-done
-echo "Folder sent successfully (excluding node_modules)!"
